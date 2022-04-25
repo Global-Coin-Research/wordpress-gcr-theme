37d5904c1f77827659452ab55d690e8da28ca3b2
Palash <bala.palash@gmail.com>
2022-04-25 23:34:39 +0600

    Change the slider post summary to excerpt

--- section/post/slider-post.php
+++ section/post/slider-post.php
@@ -1,15 +1,11 @@
-<?php 
-    $slider_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),'slider-image');
-?>
 <div class="slide">
     <div class="slide-img">
-        <!-- <img src="<?php echo $slider_image[0]; ?>" alt="<?php the_title(); ?>"> -->
         <?php the_post_thumbnail() ?>
     </div>
     <div class="slide-text">
         <p class="tag"><?php the_category(', '); ?></p>
         <h2><a href="<?php the_permalink(); ?>"><?php echo wp_trim_words( get_the_title(), 12, '' ); ?></a></h2>
-        <p><?php echo wp_trim_words(get_the_content(), 50, ' ...' ) ?></p>
+        <p><?php the_excerpt() ?></p>
         <div class="meta-data">
             <div class="meta-text">
                 <h5><?php the_author(); ?></h5>
