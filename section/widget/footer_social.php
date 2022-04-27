444841116a2f633df79b3aaef8f5fef30ca5515c
Palash <bala.palash@gmail.com>
2022-04-27 11:17:09 +0600

    Social icon order fix

--- section/widget/footer_social.php
+++ section/widget/footer_social.php
@@ -7,8 +7,8 @@
     <ul class="clearfix">
         <li><a href="https://discord.gg/globalcoinresearch" class="discord_link"><i class="discord"></i></a></li>
         <li><a href="https://twitter.com/globalcoinrsrch"><i class="icon icon-twitter"></i></a></li>
-        <li><a href="https://www.facebook.com/globalcoinresearch"><i class="icon icon-facebook"></i></a></li>
         <li><a href="https://www.youtube.com/channel/UCUOCAjg-10s_GBLchoERxzg/"><i class="icon icon-youtube-play"></i></a></li>
+        <li><a href="https://www.facebook.com/globalcoinresearch"><i class="icon icon-facebook"></i></a></li>
         <li><a href=" https://www.instagram.com/globalcoinresearch_official/"><i class="icon icon-instagram"></i></a></li>
     </ul>
 </div>
--- section/widget/footer_social_minimal.php
+++ section/widget/footer_social_minimal.php
@@ -7,8 +7,8 @@
     <ul class="clearfix">
         <li><a class="discord_link" href="//discord.gg/globalcoinresearch"><i class="icon discord"></i></a></li>
         <li><a href="https://twitter.com/globalcoinrsrch"><i class="icon icon-twitter"></i></a></li>
-        <li><a href="https://www.facebook.com/globalcoinresearch"><i class="icon icon-facebook"></i></a></li>
         <li><a href="https://www.youtube.com/channel/UCUOCAjg-10s_GBLchoERxzg/"><i class="icon icon-youtube-play"></i></a></li>
+        <li><a href="https://www.facebook.com/globalcoinresearch"><i class="icon icon-facebook"></i></a></li>
         <li><a href="https://www.instagram.com/globalcoinresearch_official/"><i class="icon icon-instagram"></i></i></a></li>
     </ul>
 </div>
