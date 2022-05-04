1b27b3a85fbdafe1c737ff0fd87fe218b844d3d3
Palash <bala.palash@gmail.com>
2022-05-04 10:18:59 +0600

    Add fan board summary on landing page

--- section/cta/fan-board.php
+++ section/cta/fan-board.php
@@ -1,7 +1,10 @@
 <div class="fan-board mt0">
     <div class="container">
         <div class="row clearfix">
-            <a class="btn btn-primary btn-large" href="https://globalcoinresearch.com/fan-board/">GCR Fan Board</a>
+            <?php 
+                if ( function_exists( 'envira_gallery' ) ) { envira_gallery( 'fan-board-summary', 'slug' ); }
+            ?>
+            <a class="btn btn-primary btn-large" href="fan-board">GCR Fan Board</a>
         </div>
     </div>
 </div>
