7e27567af34a6c4dd0be143581629348c1926a9a
Palash <bala.palash@gmail.com>
2022-05-04 10:25:24 +0600

    fanboard adjustment

--- section/cta/fan-board.php
+++ section/cta/fan-board.php
@@ -1,10 +1,13 @@
 <div class="fan-board mt0">
     <div class="container">
         <div class="row clearfix">
+            <div class="header section-title">
+                <h2>GCR Fan Board</h2>
+            </div>
             <?php 
                 if ( function_exists( 'envira_gallery' ) ) { envira_gallery( 'fan-board-summary', 'slug' ); }
             ?>
-            <a class="btn btn-primary btn-large" href="fan-board">GCR Fan Board</a>
+            <a class="btn btn-primary btn-large" href="fan-board">Check them all</a>
         </div>
     </div>
 </div>
