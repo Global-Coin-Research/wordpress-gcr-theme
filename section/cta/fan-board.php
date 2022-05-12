f2d714947a460fdcee030249bcb104aa0076abd8
Palash <bala.palash@gmail.com>
2022-05-12 23:32:14 +0600

    shortcode change

--- section/cta/fan-board.php
+++ section/cta/fan-board.php
@@ -4,7 +4,10 @@
             <div class="header section-title">
                 <h2>GCR Fan Board</h2>
             </div>
-            [lightgallery id="114"]
+            <?php 
+                echo do_shortcode('[lightgallery id="114"]');
+            ?>
+            
             <a class="btn btn-primary btn-large" href="fan-board">Check them all</a>
         </div>
     </div>
