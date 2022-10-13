8941aedb8eed05f8958a6ede0fc9bf5780ab15b3
Palash Bala <bala.palash@gmail.com>
2022-10-13 20:27:25 +0600

    Favicon sorting

--- js/theme.js
+++ js/theme.js
@@ -75,10 +75,10 @@ jQuery(document).ready(function($){
         
         // change favicon
         var link = document.querySelector("link[rel~='icon']");   
-        if(link.href.indexOf( "black" ) ===-1){
-            link.href = 'https://dev.globalcoinresearch.com/wp-content/themes/wordpress-gcr-theme/img/favicon-black.ico';
-        }else if(link.href.indexOf( "blue" ) ===-1){
-            link.href = 'https://dev.globalcoinresearch.com/wp-content/themes/wordpress-gcr-theme/img/favicon-blue.ico';
+        if(link.href.indexOf( "day" ) ===-1){
+            link.href = 'https://dev.globalcoinresearch.com/wp-content/themes/wordpress-gcr-theme/img/favicon-day.ico';
+        }else if(link.href.indexOf( "night" ) ===-1){
+            link.href = 'https://dev.globalcoinresearch.com/wp-content/themes/wordpress-gcr-theme/img/favicon-night.ico';
         }
     });
     
