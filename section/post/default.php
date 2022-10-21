5b1b4d39aba309db8b98c2ce1dadcb3825f957bd
Palash Bala <bala.palash@gmail.com>
2022-10-21 21:40:24 +0600

    Add trailing dots after the excerpt

--- section/post/default.php
+++ section/post/default.php
@@ -5,10 +5,10 @@
     <div class="text">
         <p class="tag"><?php the_category(' '); ?></p>
         <h3 class="headline">
-            <a href="<?php the_permalink(); ?>"><?php echo wp_trim_words( get_the_title(), 5, '...' ); ?></a>
+            <a href="<?php the_permalink(); ?>"><?php echo wp_trim_words( get_the_title(), 5, ' ...' ); ?></a>
         </h3>
         <div class="details">
-            <p><?php echo wp_trim_words(get_the_content(), 25, '' ) ?></p>
+            <p><?php echo wp_trim_words(get_the_content(), 25, ' ...' ) ?></p>
         </div>
         <div class="meta-data">
             <div class="meta-text">
