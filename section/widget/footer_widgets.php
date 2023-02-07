c3c73d1a05677947024711cfbf4c193d624112a2
Palash Bala <bala.palash@gmail.com>
2023-02-08 00:10:33 +0600

    update footer widget link

--- section/widget/footer_widgets.php
+++ section/widget/footer_widgets.php
@@ -19,9 +19,6 @@
         <ul>
             <li><a href="https://globalcoinresearch.com/2023/02/07/calling-for-submissions-for-the-gcr-research-pool/">Research Pool Instructions</a></li>
             <li><a href="https://forms.gle/PBeT9uhDhbb31BAaA">Join Research Pool</a></li>
-            <li><a href="https://globalcoinresearch.com/how-to-submit-contributor-posts/gcr-article-submission-guidelines/">GCR Media Mining Instructions</a></li>
-            <li><a href="https://globalcoinresearch.com/wp-admin/edit.php?post_type=post">My Posts</a></li>
-            <li><a href="https://globalcoinresearch.com/wp-admin/post-new.php">Write New Post</a></li>
         </ul>
     </div>
 </div>
