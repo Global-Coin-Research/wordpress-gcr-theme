9a749881071fc49f9decb05e13b91dad9d851286
Palash Bala <bala.palash@gmail.com>
2023-02-11 18:43:47 +0600

    update search result page design

--- search.php
+++ search.php
@@ -10,16 +10,12 @@ get_template_part('section/navigation-main')
             <div class="content search-content">
 				<h2 class="search-title">Search result for <span class="searchterm"><?php echo get_search_query(); ?></span></h2>
 				<?php if ( have_posts() ) : ?>
-					<?php 
-					while(have_posts()) : the_post();
-					$small_thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),'post-thumb-small');
-					?>
-						<div class="search-item">
-							<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-							<a href="<?php the_permalink() ?>"><img class="image" src="<?php echo $small_thumb[0]; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>"></a>
-							<p><?php echo wp_trim_words(get_the_content(), 40, '' ) ?>... [<a href="<?php the_permalink() ?>">Read More >></a>]</p>
-						</div>
-					<?php endwhile; ?>
+					<div class="two-column">
+						<?php while(have_posts()) : the_post(); ?>
+							<?php get_template_part('section/post/default') ?>
+						<?php endwhile; ?>
+					</div>
+					<?php get_template_part('section/pagination') ?>
 				<?php else : ?>
 				<div class="no-result">
 					<p>No result found of <span class="searchterm"><?php echo get_search_query(); ?></span></p>
