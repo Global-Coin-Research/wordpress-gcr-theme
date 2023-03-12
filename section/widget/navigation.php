524512f171a4ba94b4a13c4ff2906c62ceb71079
Palash Bala <bala.palash@gmail.com>
2023-03-12 19:22:19 +0600

    update navigation

--- section/widget/navigation.php
+++ section/widget/navigation.php
@@ -2,7 +2,7 @@
     wp_nav_menu( array(
         'theme_location' => 'navigation',
         'container' => 'nav',
-        'container_class' => 'navigation',
+        'container_class' => 'navigation navigation-line',
         'container_id' => 'navigation',
         'fallback_cb' => false
     ) )
