c671bafbfb5c478461e9b5ebe2025d3c28a6d9ff
Palash Bala <bala.palash@gmail.com>
2023-04-07 22:43:58 +0600

    add eggs and fix testimonial space

--- section/widget/testimonial-slideshow.php
+++ section/widget/testimonial-slideshow.php
@@ -1,35 +1,14 @@
-<!-- <div class="testimonial-slideshow-section">
-    <div class="container">
-        <div class="row"> -->
-            <!-- <div class="heading">
-                <h3>GCR has deployed $41M+ in community-sourced capital across 65+ projects to-date</h3>
-            </div> -->
-            <div id="testimonial-slideshow" class="testimonial-slideshow clearfix">
-                <a class="slide" href="/testimonials">
-                    <div class="user-image">
-                        <img class="lazy" src="<?php echo get_template_directory_uri(); ?>/img/testimonials/user.png" alt=""/>
-                    </div>
-                    <div class="user-info">
-                        <h4>Sweatcoin</h4>
-                        <p>Investor Since 2018</p>
-                    </div>
-                    <blockquote class="testimonial">
-                        <p>Look forward to it [GCR x Sweatcoin AMA]!!! @Globalcoinrsrch has been really helpful as an investor!</p>
-                    </blockquote>
-                </a>
-                <a class="slide" href="/testimonials">
-                    <div class="user-image">
-                        <img class="lazy" src="<?php echo get_template_directory_uri(); ?>/img/testimonials/user.png" alt=""/>
-                    </div>
-                    <div class="user-info">
-                        <h4>Sweatcoin</h4>
-                        <p>Investor Since 2018</p>
-                    </div>
-                    <blockquote class="testimonial">
-                        <p>Look forward to it [GCR x Sweatcoin AMA]!!! @Globalcoinrsrch has been really helpful as an investor!</p>
-                    </blockquote>
-                </a>
-            </div>
-        <!-- </div>
+<div id="testimonial-slideshow" class="testimonial-slideshow clearfix">
+    <div class="slide">
+        <div class="user-image">
+            <img class="lazy" src="<?php echo get_template_directory_uri(); ?>/img/testimonials/sweatcoin.png" alt=""/>
+        </div>
+        <div class="user-info">
+            <h4>Sweatcoin</h4>
+            <p>Investor Since 2018</p>
+        </div>
+        <blockquote class="testimonial">
+            <p>@Globalcoinrsrch has been really helpful as an investor!</p>
+        </blockquote>
     </div>
-</div> -->
+</div>
