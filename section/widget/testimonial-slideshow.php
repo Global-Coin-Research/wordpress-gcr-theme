b7a5938e148a51da4583c906d9ec7e9bd607aeb3
Palash Bala <bala.palash@gmail.com>
2023-05-01 23:30:17 +0600

    update the image path of partners logo

--- section/widget/testimonial-slideshow.php
+++ section/widget/testimonial-slideshow.php
@@ -1,7 +1,7 @@
 <div id="testimonial-slideshow" class="testimonial-slideshow clearfix">
     <a class="slide" href="https://globalcoinresearch.com/partners/">
         <div class="user-image">
-            <img class="lazy" src="<?php echo get_template_directory_uri(); ?>/img/testimonials/sweatcoin.png" alt=""/>
+            <img class="lazy" src="<?php echo get_template_directory_uri(); ?>/img/partners/sweatcoin.png" alt=""/>
         </div>
         <div class="user-info">
             <h4>Sweatcoin</h4>
