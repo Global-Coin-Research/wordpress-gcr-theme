98a227d9a9ec2572a3ade6600ddfc7e390a4e508
Palash Bala <bala.palash@gmail.com>
2023-05-12 22:40:12 +0600

    update default description tag

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo('charset') ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="GCR is a Community of Investors & Researchers in Web3">
+    <meta name="description" content="Global Coin Research is a Community-led DAO for Web3 Research & Investing. We source, vet & fund Pre-Seed to Series A opportunities. Join to Invest Together!">
     <meta name="twitter:data1" content="GCR">
     <link rel="icon" href="<?php echo get_template_directory_uri() ?>/img/favicon-day.ico" type="image/x-icon">
     <?php wp_head(); ?>
