b75fec92daffd4d8146bc7a31d3e952e854dfc70
Palash Bala <bala.palash@gmail.com>
2023-05-13 11:21:23 +0600

    Add google tracking code on header

--- header.php
+++ header.php
@@ -7,6 +7,17 @@
     <meta name="description" content="Global Coin Research is a Community-led DAO for Web3 Research & Investing. We source, vet & fund Pre-Seed to Series A opportunities. Join to Invest Together!">
     <meta name="twitter:data1" content="GCR">
     <link rel="icon" href="<?php echo get_template_directory_uri() ?>/img/favicon-day.ico" type="image/x-icon">
+    
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HTLNZHLK62"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-HTLNZHLK62');
+    </script>
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
