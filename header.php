9c5f12987ed81931678ab645cbdb202bad6d46d3
Palash Bala <bala.palash@gmail.com>
2023-05-16 16:27:06 +0600

    Update analytics

--- header.php
+++ header.php
@@ -8,15 +8,17 @@
     <meta name="twitter:data1" content="GCR">
     <link rel="icon" href="<?php echo get_template_directory_uri() ?>/img/favicon-day.ico" type="image/x-icon">
     
+    
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HTLNZHLK62"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VP9LKKG4GN"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-HTLNZHLK62');
+    gtag('config', 'G-VP9LKKG4GN');
     </script>
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
