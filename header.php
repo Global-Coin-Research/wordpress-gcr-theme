3e1fd97c3a2451f864160510dd49b4274a31d2ba
Palash Bala <bala.palash@gmail.com>
2023-05-16 15:09:12 +0600

    Update tag

--- header.php
+++ header.php
@@ -9,15 +9,14 @@
     <link rel="icon" href="<?php echo get_template_directory_uri() ?>/img/favicon-day.ico" type="image/x-icon">
     
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VP9LKKG4GN"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HTLNZHLK62"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-VP9LKKG4GN');
+    gtag('config', 'G-HTLNZHLK62');
     </script>
-
 
     <?php wp_head(); ?>
 </head>
