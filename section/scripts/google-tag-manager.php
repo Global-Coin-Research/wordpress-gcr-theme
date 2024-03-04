287dd0d006e1e17c67ed90adba23405a7eb0e619
Palash Bala <bala.palash@gmail.com>
2024-03-04 23:32:08 +0600

    Update GA

--- section/scripts/google-tag-manager.php
+++ section/scripts/google-tag-manager.php
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VP9LKKG4GN"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-G3YGQYB74Q"></script>
 <script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-gtag('config', 'G-VP9LKKG4GN');
+  gtag('config', 'G-G3YGQYB74Q');
 </script>
